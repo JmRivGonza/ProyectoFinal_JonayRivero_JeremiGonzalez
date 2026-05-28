65935c47d5b172126dc525d2fc4f52f767241c3c
Jeremi González Perera <gonzalezjeremi532@gmail.com>
2026-05-28 16:37:49 +0100

    Subida de configuracion Excepciones

--- Gimnasio/src/main/java/com/dam/userapp/Exception/GlobalExceptionHandler.java
+++ Gimnasio/src/main/java/com/dam/userapp/Exception/GlobalExceptionHandler.java
@@ -2,23 +2,28 @@ package com.dam.userapp.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGeneralExceptions(Exception ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleGeneralExceptions(MethodArgumentNotValidException ex) {
+        List<String> errores = ex.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + " : " + error.getDefaultMessage())
+        .toList();
+        
         Map<String, Object> respuestaError = new HashMap<>();
             respuestaError.put("timestamp", LocalDateTime.now());
             respuestaError.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
             respuestaError.put("error", "Error Interno del Servidor");
-            respuestaError.put("details", ex.getMessage());
+            respuestaError.put("detalles", errores);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(respuestaError);
     }   
-
     
 }
